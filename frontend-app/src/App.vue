67ae989616ddde0e2cb740fdf74965d924e3a274
noaman <noaman@savitriya.com>
2023-11-15 12:00:30 +0530

    Tested frontend for crud operations of backend

---
+++ frontend-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <TaskComponent />
+  </div>
+</template>
+
+<script>
+import TaskComponent from './components/TaskComponent.vue';
+
+export default {
+  components: {
+    TaskComponent,
+  },
+};
+</script>
+
+<style>
+/* Add your global styles here */
+</style>
