e264cc947cda03cdcb4ddf7af54aad700c1a0bca
noaman <noaman@savitriya.com>
2023-11-16 11:17:44 +0530

    configure prod.env.js

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,4 +1,7 @@
-'use strict'
+'use strict';
+
 module.exports = {
-  NODE_ENV: '"production"'
-}
+  NODE_ENV: '"production"',
+  ROOT_API: '"http://nova.local.com/tasks"',
+  FRONT_ROOT_URL: '"http://nova.local.com/tasks/api"',
+};
