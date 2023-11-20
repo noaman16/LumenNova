8dc84785aaaa1b0a1809e02cc0ff2ab6ef75b6bb
noaman <noaman@savitriya.com>
2023-11-20 15:43:43 +0530

    Created a AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return response()->json(['token' => Auth::user()->id]);
+        }
+
+        return response()->json(['error' => 'Invalid credentials'], 401);
+    }
+
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'username' => 'required|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $data['password'] = bcrypt($data['password']);
+
+        $user = User::create($data);
+
+        return response()->json(['token' => $user->id]);
+    }
+}
