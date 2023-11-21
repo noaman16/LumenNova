4f6bce06b20d12ae1862b74856dd582c829cb88a
noaman <noaman@savitriya.com>
2023-11-21 15:04:58 +0530

    implement datatable in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,48 @@
 <template>
-  <div id="app">
-    <TaskList />
+  <div id="app" class="container">
+    <h1 class="text-center">Datatable with Vue</h1>
+
+    <table id="myTable" class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Description</th>          
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import TaskList from '@/components/TaskList';
+import axios from "axios";
+import $ from "jquery";
 
 export default {
-  components: {
-    TaskList
-  }
+  mounted() {
+    this.getUsers();
+  },
+  methods: {
+    getUsers() {
+      axios
+        .get("http://nova.local.com/tasks")
+        .then((response) => {
+          $("#myTable").DataTable({
+            dom: "Bfrtip",
+            buttons: ["colvis", "excel", "print", "csv"],
+            data: response.data,
+            columns: [
+              { data: "id" },
+              { data: "title" },
+              { data: "description" },              
+            ],
+          });
+        })
+        .catch((error) => console.log(error.response));
+    },
+  },
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
