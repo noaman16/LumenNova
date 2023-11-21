55b2f39c25a46e122ebb37d76491fe3989065e2c
noaman <noaman@savitriya.com>
2023-11-21 15:06:12 +0530

    import css  etc from installed packages

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,38 @@
-import Vue from 'vue';
-import App from './App';
-import axios from 'axios';
+// import Vue from 'vue';
+// import App from './App';
+// import axios from 'axios';
+
+// Vue.config.productionTip = false;
+
+// // Set the base URL for Axios
+// axios.defaults.baseURL = 'http://nova.local.com:80';
+
+// Vue.prototype.$axios = axios;
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app');
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "datatables.net-bs4";
+import "datatables.net-bs4/css/dataTables.bootstrap4.min.css";
+import "jszip";
+import "pdfmake";
+import "datatables.net-buttons-bs4";
+import "datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css";
+import "datatables.net-buttons/js/buttons.colVis";
+import "datatables.net-buttons/js/buttons.flash";
+import "datatables.net-buttons/js/buttons.html5";
+import "datatables.net-buttons/js/buttons.print";
+
+
 
 Vue.config.productionTip = false;
 
-// Set the base URL for Axios
-axios.defaults.baseURL = 'http://nova.local.com:80';
-
-Vue.prototype.$axios = axios;
-
 new Vue({
-  render: h => h(App),
-}).$mount('#app');
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
