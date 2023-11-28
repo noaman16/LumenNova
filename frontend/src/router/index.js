26c84fcd90ccedee3fce0a7576d36bc15f7a3f32
noaman <noaman@savitriya.com>
2023-11-28 17:51:32 +0530

    creted DataTable.vue component Need to define new route for displaying dataTable. dataTable which also configure server side. facing some chalanges. tried to resolve it.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+// import HelloWorld from '@/components/HelloWorld';
 import TaskList from '@/components/TaskList';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Home',
+    //   component: HelloWorld
+    // },
     {
       path: '/',
-      name: 'TaskList',
+      name: 'Tasklist',
       component: TaskList
     }
   ]
