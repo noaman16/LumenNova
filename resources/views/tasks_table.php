5c92e3043393d806502282ce52b0c4eb8786a8ee
noaman <noaman@savitriya.com>
2023-11-28 16:37:40 +0530

    style: Improve DataTable layout and appearance
    - Added Bootstrap container to center the table on the page.
    - Adjusted body padding for improved spacing.
    - Added custom styles to the table wrapper for better alignment.
    - Updated the head section to include necessary stylesheets.
    
    This commit enhances the visual presentation of the DataTable on desktop screens.

--- resources/views/tasks_table.php
+++ resources/views/tasks_table.php
@@ -4,30 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tasks DataTable</title>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap4.min.css">
+    <style>
+        /* Add custom styles to adjust the table layout */
+        body {
+            padding: 20px; /* Add padding to the body */
+        }
+        #tasks-table_wrapper {
+            margin-top: 20px; /* Add margin to the top of the table wrapper */
+        }
+    </style>
 </head>
 <body>
 
-    <table id="tasks-table" class="table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Description</th>
-                <!-- Add more columns if needed -->
-            </tr>
-        </thead>
-    </table>
-    
+    <div class="container">
+        <table id="tasks-table" class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <!-- Add more columns if needed -->
+                </tr>
+            </thead>
+        </table>
+    </div>
 
- 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap4.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
 
     <script>
         $(document).ready(function() {
@@ -39,7 +46,6 @@
                     { data: 'description' },
                     // Add more columns if needed
                 ]
-                
             });
         });
     </script>
