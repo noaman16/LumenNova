a44503ed7f39f3e261f71641006624730737935e
noaman <noaman@savitriya.com>
2023-11-28 16:31:07 +0530

    Update view for manage view of datatable

---
+++ resources/views/tasks_table.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tasks DataTable</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+</head>
+<body>
+
+    <table id="tasks-table" class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Description</th>
+                <!-- Add more columns if needed -->
+            </tr>
+        </thead>
+    </table>
+    
+
+ 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+
+    <script>
+        $(document).ready(function() {
+            $('#tasks-table').DataTable({
+                ajax: '/tasks_table',
+                columns: [
+                    { data: 'id' },
+                    { data: 'title' },
+                    { data: 'description' },
+                    // Add more columns if needed
+                ]
+                
+            });
+        });
+    </script>
+
+</body>
+</html>
