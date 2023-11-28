3ad44bd79b9d0a00a1815526ccbf4089eaed9323
noaman <noaman@savitriya.com>
2023-11-28 17:48:47 +0530

    Update index.js. Tried to change route of tasklist.  issue occured. temporary comment out section and also remove from App.vue which recently add regarding to helloWorld

---
+++ frontend/src/components/DataTable.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>Data Table</h2>
+    <table id="dataTable">
+      <!-- DataTable content goes here -->
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // DataTable options or configurations
+    };
+  },
+  mounted() {
+    // Fetch data from the server and initialize the DataTable
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      // Make an API request to fetch data from the server
+      // Example using Axios
+      // You may need to adjust this based on your server-side implementation
+      this.$axios.get('/tasks_table_view').then(response => {
+        // Initialize the DataTable with the fetched data
+        $('#dataTable').DataTable({
+          data: response.data,
+          columns: [
+            // Define your DataTable columns based on your data structure
+            { data: 'id', title: 'ID' },
+            { data: 'title', title: 'Title' },
+            { data: 'description', title: 'Description' },
+            // ... other columns
+          ],
+        });
+      }).catch(error => {
+        console.error('Error fetching data:', error);
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add any component-specific styles if needed */
+</style>
