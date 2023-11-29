82bc30d769065918bbc095c3e96a725c90a4af74
noaman <noaman@savitriya.com>
2023-11-29 18:59:02 +0530

    Update tasks_table.php This file works. just need to proper formmatting

--- resources/views/tasks_table.php
+++ resources/views/tasks_table.php
@@ -1,63 +1,63 @@
+<!-- resources/views/table.blade.php -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tasks DataTable</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.0.0/css/buttons.dataTables.min.css">
-    <!-- Include other necessary stylesheets manually -->
-
-    <style>
-        /* Add custom styles to adjust the table layout */
-        body {
-            padding: 20px; /* Add padding to the body */
-        }
-        #tasks-table_wrapper {
-            margin-top: 20px; /* Add margin to the top of the table wrapper */
-        }
-    </style>
+    <title>Task Table</title>
+    <!-- Include Bootstrap CSS and DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.bootstrap4.min.css">
 </head>
 <body>
 
-    <div class="container">
-        <table id="tasks-table" class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <!-- Add more columns if needed -->
-                </tr>
-            </thead>
-        </table>
-    </div>
+<div class="container mt-5">
+    <table id="myTable" class="table table-bordered">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+        </tr>
+        </thead>
+        <tbody>
+        <!-- The data will be dynamically populated here -->
+        </tbody>
+    </table>
+</div>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap4.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.0.0/js/dataTables.buttons.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.0.0/js/buttons.html5.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- Include jQuery, Bootstrap JS, DataTables JS, and DataTables Buttons JS -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.bootstrap4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.colVis.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.flash.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
 
-    <script>
-        $(document).ready(function() {
-            $('#tasks-table').DataTable({
-                ajax: '/tasks_table',
-                columns: [
-                    { data: 'id' },
-                    { data: 'title' },
-                    { data: 'description' },
-                    // Add more columns if needed
-                ],
-                dom: 'Bfrtip',
-                buttons: [
-                    'csv', 'excel', 'print', 'colvis'
-                ]
-            });
+<script>
+    $(document).ready(function () {
+        $('#myTable').DataTable({
+            dom: 'Bfrtip',
+            buttons: ['colvis', 'excel', 'print', 'csv'],
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: '/tasks_table', // Update this with your Lumen route for fetching tasks
+                type: 'GET',
+            },
+            columns: [
+                {data: 'id'},
+                {data: 'title'},
+                {data: 'description'},
+            ],
         });
-    </script>
+    });
+</script>
 
 </body>
 </html>
