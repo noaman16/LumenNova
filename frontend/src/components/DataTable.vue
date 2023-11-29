0183053e99c5e545d47dfba7aa8588f6d25eaeb2
noaman <noaman@savitriya.com>
2023-11-29 11:11:23 +0530

    update

--- frontend/src/components/DataTable.vue
+++ frontend/src/components/DataTable.vue
@@ -1,70 +1,48 @@
 <template>
-  <div class="datatable">
-    <h2>Datatable</h2>
-    <table id="taskDataTable" class="display">
+  <div>
+    <table id="myTable" class="table table-bordered mt-5">
       <thead>
         <tr>
           <th>ID</th>
           <th>Title</th>
           <th>Description</th>
-          <!-- Add more columns as needed -->
         </tr>
       </thead>
-      <tbody>
-        <tr v-for="row in dataTableRows" :key="row.id">
-          <td>{{ row.id }}</td>
-          <td>{{ row.title }}</td>
-          <td>{{ row.description }}</td>
-          <!-- Add more columns as needed -->
-        </tr>
-      </tbody>
+      <tbody></tbody>
     </table>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import $ from "jquery";
+
 export default {
-  data() {
-    return {
-      dataTableRows: [],
-    };
-  },
   mounted() {
-    this.fetchDataTable();
+    this.getUsers();
   },
   methods: {
-    async fetchDataTable() {
-      try {
-        const response = await this.$axios.get('/tasks_table_view');
-        this.dataTableRows = response.data.data; // Adjust the property as per your server response
-        this.initializeDataTable();
-      } catch (error) {
-        console.error('Error fetching DataTable:', error);
-      }
-    },
-    initializeDataTable() {
-      // Destroy any existing DataTable
-      if ($.fn.DataTable.isDataTable('#taskDataTable')) {
-        $('#taskDataTable').DataTable().destroy();
-      }
-
-      // Initialize DataTable
-      $(document).ready(() => {
-        $('#taskDataTable').DataTable({
-          data: this.dataTableRows,
-          columns: [
-            { data: 'id', title: 'ID' },
-            { data: 'title', title: 'Title' },
-            { data: 'description', title: 'Description' },
-            // Add more columns as needed
-          ],
-        });
-      });
+    getUsers() {
+      axios
+        .get("http://nova.local.com/tasks")
+        .then((response) => {
+          $("#myTable").DataTable({
+            dom: "Bfrtip",
+            buttons: ["colvis", "excel", "print", "csv"],
+            data: response.data,
+            columns: [
+              { data: "id" },
+              { data: "title" },
+              { data: "description" },
+            ],
+          });
+        })
+        .catch((error) => console.log(error.response));
     },
   },
 };
 </script>
 
 <style scoped>
-/* Add any additional styles for your DataTable if needed */
+/* Add any component-specific styles here */
 </style>
