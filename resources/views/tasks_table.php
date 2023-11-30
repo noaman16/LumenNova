ea3f10876be88d95d473669de6ee0a86c2ddd2be
noaman <noaman@savitriya.com>
2023-11-30 10:34:21 +0530

    Enhance table view with column filtering in tasks_table.php
    Integrate DataTables Column Filtering extension to enable column-wise filtering. Added input fields in the table footer for each column to facilitate user-friendly searching. Updated the DataTable initialization to include necessary dependencies for column reordering and fixed columns.

--- resources/views/tasks_table.php
+++ resources/views/tasks_table.php
@@ -10,6 +10,9 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.bootstrap4.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Include DataTables Column Filtering CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/colreorder/1.5.5/css/colReorder.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/fixedcolumns/3.3.3/css/fixedColumns.bootstrap4.min.css">
 </head>
 <body>
 
@@ -24,6 +27,13 @@
                     <th>Description</th>
                 </tr>
                 </thead>
+                <tfoot>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                </tr>
+                </tfoot>
                 <tbody>
                 <!-- The data will be dynamically populated here -->
                 </tbody>
@@ -44,10 +54,13 @@
 <script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!-- Include DataTables Column Filtering JS -->
+<script src="https://cdn.datatables.net/colreorder/1.5.5/js/dataTables.colReorder.min.js"></script>
+<script src="https://cdn.datatables.net/fixedcolumns/3.3.3/js/dataTables.fixedColumns.min.js"></script>
 
 <script>
     $(document).ready(function () {
-        $('#myTable').DataTable({
+        var table = $('#myTable').DataTable({
             dom: 'Bfrtip',
             buttons: ['colvis', 'excel', 'print', 'csv'],
             processing: true,
@@ -61,6 +74,25 @@
                 {data: 'title'},
                 {data: 'description'},
             ],
+            colReorder: true, // Enable column reordering
+            fixedColumns: true, // Enable fixed columns
+        });
+
+        // Add column filtering inputs
+        $('#myTable tfoot th').each(function () {
+            var title = $(this).text();
+            $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+        });
+
+        // Apply the search
+        table.columns().every(function () {
+            var that = this;
+
+            $('input', this.footer()).on('keyup change', function () {
+                if (that.search() !== this.value) {
+                    that.search(this.value).draw();
+                }
+            });
         });
     });
 </script>
