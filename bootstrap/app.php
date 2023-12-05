4fc6ae3717ec4bf16327b0962dedc5ce30c39838
noaman <noaman@savitriya.com>
2023-12-05 12:06:11 +0530

    Added comments for /bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,16 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| Here we will load the environment and create the application instance
+| that serves as the central piece of this framework. We'll use this
+| application as an "IoC" container and router for this framework.
+|
+*/
 header('Access-Control-Allow-Origin:  *');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
@@ -16,11 +26,25 @@ $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
+//  $app->withFacades();
+// // class_alias('Illuminate\Support\Facades\Route', 'Route');
+
 $app->withFacades(true, [
     'Yajra\DataTables\Facades\DataTables' => 'DataTables',
 ]);
 
-$app->withEloquent();
+ $app->withEloquent();
+
+/*
+|--------------------------------------------------------------------------
+| Register Container Bindings
+|--------------------------------------------------------------------------
+|
+| Now we will register a few bindings in the service container. We will
+| register the exception handler and the console kernel. You may add
+| your own bindings here if you like or you can make another file.
+|
+*/
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
@@ -32,8 +56,33 @@ $app->singleton(
     App\Console\Kernel::class
 );
 
+/*
+|--------------------------------------------------------------------------
+| Register Config Files
+|--------------------------------------------------------------------------
+|
+| Now we will register the "app" configuration file. If the file exists in
+| your configuration directory it will be loaded; otherwise, we'll load
+| the default version. You may register other files below as needed.
+|
+*/
+
 $app->configure('app');
-$app->configure('datatables'); // Add this line
+$app->configure('datatables');
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Register Middleware
+|--------------------------------------------------------------------------
+|
+| Next, we will register the middleware with the application. These can
+| be global middleware that run before and after each request into a
+| route or middleware that'll be assigned to some specific routes.
+|
+*/
 
 $app->middleware([
     App\Http\Middleware\ExampleMiddleware::class
@@ -43,11 +92,31 @@ $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
 ]);
 
+/*
+|--------------------------------------------------------------------------
+| Register Service Providers
+|--------------------------------------------------------------------------
+|
+| Here we will register all of the application's service providers which
+| are used to bind services into the container. Service providers are
+| totally optional, so you are not required to uncomment this line.
+|
+*/
 $app->register(Yajra\DataTables\DataTablesServiceProvider::class);
-
 $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
+
+/*
+|--------------------------------------------------------------------------
+| Load The Application Routes
+|--------------------------------------------------------------------------
+|
+| Next we will include the routes file so that they can all be added to
+| the application. This will provide all of the URLs the application
+| can respond to, as well as the controllers that may handle them.
+|
+*/
 
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
