38d58c91db8389021f7fbbd4fb463d5450e9a465
noaman <noaman@savitriya.com>
2023-12-14 12:26:29 +0530

    initiated authentication. installed composer require laravel/passport

---
+++ resources/views/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+
+    @if($errors->has('login'))
+        <div>{{ $errors->first('login') }}</div>
+    @endif
+
+    <form action="{{ http://nova.local,com('/login') }}" method="post">
+        @csrf
+        <label>Email: <input type="email" name="email" required></label><br>
+        <label>Password: <input type="password" name="password" required></label><br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
