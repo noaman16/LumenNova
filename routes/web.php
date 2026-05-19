49f5907b6162b6ffdaa8bbaae72f3f62fee6ce17
Mansuri Noaman <136847939+noaman16@users.noreply.github.com>
2026-05-19 14:07:53 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ $router->options('/{any:.*}', function () {
     return response('OK', 200)
         ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
         ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
-});
+
 
 
 
